513a07222b2f3e2f5492087a69ca986f7665af3e
siddharthN23 <siddharthnavasundi@gmail.com>
2025-05-01 14:22:07 +0530

    Add files via upload

---
+++ task1task2.py
@@ -0,0 +1,13 @@
+
+#TASK 1:PERFORMING BASIC OPERATIOINS....
+x=int(input('enter the first number :'))
+y=int(input('enter the second number:'))
+print('Addition=',x+y)
+print('subtraction:',x-y)
+print('Multiplication:',x*y)
+print('Division:',x/y)
+
+#TASK 2:PERSONALIZED GREETING...
+first_name=input('Enter your first name:')
+last_name=input('Enter your last name:')
+print('Hello',first_name+ last_name,'! Welcome to the python program')
